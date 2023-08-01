390263263dc65d184ffd3b277b18ef0cdcd97149
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-08-01 22:56:45 +0300

    Created a cart component and styled it

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,29 @@
 function Cart() {
 
+    return(
+        <>
+            <div className="cart">
+                <h2 className="cart-title">Your Orders</h2>
+                <div className="cart-content">
+                    <div className="cart-box">
+                        <img src="https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/n3frruwjijq8wanzdzrz" alt="" className="cart-img"/>
+                        <div className="detail-box">
+                            <div className="food-title">King Savers</div>
+                            <div className="food-price">Ksh 660</div>
+                        </div>
+                        <span><i className='bx bxs-trash cart-remove'></i></span>
+                    </div>
+                   <div className="total">
+                    <div className="total-title">Total</div>
+                    <div className="total-price">Ksh</div>
+                    </div> 
+                    <button type="button" className="btn-buy">Pay</button>
+                    <span><i class='bx bxs-x-circle' id="close-cart"></i></span>
+                </div>
+                
+            </div>
+        </>
+    )
 }
 
 export default Cart;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,19 @@
-import React from "react";
+import Cart from './Cart' 
 
-function NavBar() {
+function NavBar(){
 
     return(
-        <>
-            
-        </>
+        <header>
+            <div className="nav container">
+                <h1 className="logo">Flavour Plaza</h1>
+                <span><i class='bx bx-shopping-bag' id="cart-icon" data-quantity="0"></i></span>
+            </div>
+            <Cart/>
+        </header>
     )
+    
+        
+    
 }
 
 export default NavBar;
-
-
