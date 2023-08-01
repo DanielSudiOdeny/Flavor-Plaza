9671f9468bb9ce322fcdd78ef51285b572306a68
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-08-01 19:50:31 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,25 @@
 import { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import Menu from "./Menu";
-function App() {}
+import Cart from "./Cart";
+import '../App.css'
+
+function App() {
+    const [food, setFood] = useState([]);
+  
+    useEffect(() => {
+      fetch("http://localhost:5000/food")
+        .then((res) => res.json())
+        .then((data) => setFood(data));
+    }, []);
+    
+    return(
+       <>
+        <NavBar/>
+        <Menu food={food}/>
+        <Cart food={food}/>
+       </>
+    )
+}
 
 export default App;
