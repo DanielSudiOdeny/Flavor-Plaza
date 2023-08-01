08ed6fc7d915fddfe6fd58ac0f57969c4881ac7e
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-08-01 23:27:51 +0300

    Updated App.css

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,29 +1,48 @@
-function Food({ food, onReduce, onAdd, quantity, price}) {
-    return (
-      <li className="food">
-        <img className="img" src={food.photoName} alt={food.name} />
-        <div>
-          <h5>{food.name}</h5>
-          {/* <p>{food.ingredients}</p> */}
-          <span>Ksh {price}</span>
-          <span className="quantity-btn">
-            <button
-              className="btn-quantity-reduce btn-quantity"
-              onClick={onReduce}
-            >
-              -
-            </button>
-            <span className="btn">{quantity}</span>
-            <button className="btn-quantity-add btn-quantity" onClick={onAdd}>
-              +
-            </button>
-          </span>
-            <button className="order-btn">Add to Order</button>
-           
-        </div>
-      </li>
-    );
+import { useState, useEffect } from "react";
+
+function Food({ food }) {
+  const [quantity, setQuantity] = useState(1);
+
+  function handleOnAddQuantity() {
+    setQuantity((q) => q + 1);
   }
-  
-  export default Food;
-  
+
+  function handleOnReduceQuantity() {
+    if (quantity >= 2) setQuantity((q) => q - 1);
+  }
+
+  const [totalPrice, setTotalPrice] = useState(food.price);
+
+  useEffect(() => {
+    setTotalPrice(food.price * quantity);
+  }, [food.price, quantity]);
+  return (
+    <li className="food">
+      <img className="" src={food.photoName} alt={food.name} />
+      <div>
+        <h5>{food.name}</h5>
+
+        <span>Ksh {totalPrice}</span>
+        <span className="quantity-btn">
+          <button
+            className="btn-quantity-reduce btn-quantity"
+            onClick={handleOnReduceQuantity}
+          >
+            -
+          </button>
+          <span className="btn">{quantity}</span>
+          <button
+            className="btn-quantity-add btn-quantity"
+            onClick={handleOnAddQuantity}
+          >
+            +
+          </button>
+        </span>
+
+        <button className="order-btn">Add to Order</button>
+      </div>
+    </li>
+  );
+}
+
+export default Food;
