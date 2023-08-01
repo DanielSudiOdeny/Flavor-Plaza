5bb1c1092fd799ce666ed1ceaa7c22fb264f1b2f
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-08-01 19:39:53 +0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,3 @@ function NavBar() {
 export default NavBar;
 
 
-
-<div className="nav-bar">
-<a href="." className="logo">Flavour Plaza</a>
-<span><i className='bx bx-shopping-bag' id="cart-icon"></i></span>
-</div>
