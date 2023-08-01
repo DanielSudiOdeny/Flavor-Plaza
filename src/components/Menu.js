2c87211f0143a86ccdb668c04345e83e8f528b0b
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-08-01 23:07:41 +0300

    Updated Menu component and style the food component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,12 @@
-import { useState } from "react";
 import Food from "./Food";
 
 function Menu({ food }) {
-  const [quantity, setQuantity] = useState(1);
-  const [price, setPrice] = useState(food.price);
-
-  function handleOnAddQuantity() {
-    setQuantity((q) => q + 1);
-    setPrice((p) => p * quantity);
-    console.log(price);
-  }
-
-  function handleOnReduceQuantity() {
-    setQuantity((q) => q - 1);
-  }
-
   console.log(food);
   return (
     <div className="menu">
       {food.map((food) => (
         <ul className="foods">
-          <Food
-            food={food}
-            quantity={quantity}
-            onAdd={handleOnAddQuantity}
-            onReduce={handleOnReduceQuantity}
-            price={price}
-          />
+          <Food food={food} />
         </ul>
       ))}
     </div>
