b44e9abacf7a473146ad79cd4d52516657df5bb4
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-08-01 21:01:43 +0300

    fetch food data from server

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,24 @@
-function App() {}
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+import Menu from "./Menu";
+
+import "../App.css";
+
+function App() {
+  const [food, setFood] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/food")
+      .then((res) => res.json())
+      .then((data) => setFood(data));
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Menu food={food} />
+    </>
+  );
+}
 
 export default App;
