c4ec6635133db7d756c8f9acc6468003f1737111
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-08-01 21:04:57 +0300

    Implement displaying every Food component using map method

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,3 +1,16 @@
-function Menu() {}
+import Food from "./Food";
+
+function Menu({ food }) {
+  console.log(food);
+  return (
+    <div className="menu">
+      {food.map((food) => (
+        <ul className="foods">
+          <Food food={food} />
+        </ul>
+      ))}
+    </div>
+  );
+}
 
 export default Menu;
